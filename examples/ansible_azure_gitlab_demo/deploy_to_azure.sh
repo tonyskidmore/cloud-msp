58f1024cdddf4833c3f947ae7c787add30425d9c
Tony Skidmore <tony@skidmore.co.uk>
2017-11-18 17:08:51 +0000

    splitting role download to avoid errors

---
+++ examples/ansible_azure_gitlab_demo/deploy_to_azure.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# workaround for playbook failing if roles are missing
+# download them first and then run the playbook
+# otherwise run the 2 commands below sequentially
+ansible-galaxy install -r roles/requirements.yml -p ./roles/ --force
+ansible-playbook azure_create_gitlab_demo.yml $1
