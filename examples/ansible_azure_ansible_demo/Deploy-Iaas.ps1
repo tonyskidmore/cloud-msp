d8b519271c5b507355fa7c31e42f34916d2096aa
Tony Skidmore <tony@skidmore.co.uk>
2018-02-04 21:18:32 +0000

    adding image code

--- examples/ansible_azure_ansible_demo/Deploy-Iaas.ps1
+++ examples/ansible_azure_ansible_demo/Deploy-Iaas.ps1
@@ -11,17 +11,29 @@ $azureAccount = Add-AzureRmAccount -Credential $cred
 
 }
 
+
 ### Define Deployment Variables
 {
 
 $location = 'UK West'
-$resourceGroupName = 'anzible-demo-iaas'
-$resourceDeploymentName = 'ansible-demo-iaas-deployment'
+$resourceGroupName = 'ansible-demo'
+$resourceDeploymentName = 'ansible-demo-deployment'
 $templatePath = 'files'
 $templateFile = 'azuredeploy.json'
 $template = Join-Path -Path $pwd -ChildPath "files\$templateFile"
 $adminUserName = "cloud_user"
 $sshKeyData = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQClumoBR/nnZfhhUFNGBtCYf1Sa4u+QzMf6OrzExNcnWxHvctdLxJozH1PZGt4CrcJ7Tp6roBXU0ZdqsYDZW+slICeIudvUerMqJEvzA7SWAEaawfenC9QJuxbzd8VOZi3DxURw6nE2mhgtFb1v4oepJkdVQFwc1PQIqE8W3jBjLguUn+TfJQpjF6aFJdDR+KQmI/DWDpsLXy2g56lIWaJP5Wv0eX4WarFQnAIN9eKn8o4CqccYwJ1a/TWyVgpFpUGU2nZoY2FF1oU+8n17RGqh3yoEoDC/c6GTgVfb3KWnx6Ai9zyj686vMnPfPca/q/YkYLUUg5Co/EQu1NwmoNKp"
+
+}
+
+### Find Images
+{
+
+Get-AzureRmVMImagePublisher -Location $location #check all the publishers available
+
+Get-AzureRmVMImageOffer -Location $location -PublisherName "OpenLogic" #look for offers for a publisher
+Get-AzureRmVMImageSku -Location $location -PublisherName "OpenLogic" -Offer "CentOS" #view SKUs for an offer
+Get-AzureRmVMImage -Location $location -PublisherName "OpenLogic" -Offer "CentOS" -Skus "7.4" -Version "7.4.20180118" #pick one!
 
 }
 
